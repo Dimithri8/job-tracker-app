2bcd9b0c6ea2f1c2ce1e47a7e44bb71ae34119ad
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-11 23:37:51 +0530

    Setup navigation links for pages

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,37 @@
 import { Box, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar({ className }) {
   return (
     <Box
       className={className}
       component={"div"}
-      sx={{ backgroundColor: "purple" }}
+      sx={{ backgroundColor: "white" }}
     >
-      <Typography>Sidebar</Typography>
+      <Typography
+        variant={"h1"}
+        sx={{ fontSize: 20, fontWeight: 600, padding: 2 }}
+      >
+        Job Tracker
+      </Typography>
+      <Box component={"div"}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={"/"}>Dashboard</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/jobs"}>Jobs</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/interviews"}>Interviews</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/analytics"}>Analytics</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Box>
     </Box>
   );
 }
