53187a26c619a597a81cdb8597334c48339d1ee9
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-11 22:32:16 +0530

    Created the dashboard component with dummy content to test the layout

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout/Layout";
+export default function Dashboard() {
+  return (
+    <Layout>
+      <div>
+        <h1>Dashboard Overview</h1>
+      </div>
+    </Layout>
+  );
+}
