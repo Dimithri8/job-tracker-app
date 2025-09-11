2b19d0de70d19c99c1cb2d9f62373387aba148e5
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-11 23:39:15 +0530

    Removed layout component wrapper

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,7 @@
-import Layout from "../../components/Layout/Layout";
 export default function Dashboard() {
   return (
-    <Layout>
-      <div>
-        <h1>Dashboard Overview</h1>
-      </div>
-    </Layout>
+    <div>
+      <h1>Dashboard Overview</h1>
+    </div>
   );
 }
---
+++ frontend/src/pages/Interviews/Interviews.jsx
@@ -0,0 +1,7 @@
+export default function Interviews() {
+  return (
+    <div>
+      <h1>Manage Interviews</h1>
+    </div>
+  );
+}
