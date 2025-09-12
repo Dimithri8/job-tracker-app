237ab3fdb12a646cbe7de87c4f008c49b0bc91c7
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-12 12:26:46 +0530

    Changes header background color and added box shadow

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 export default function Header({ className }) {
   return (
     <Box
       className={className}
       component={"div"}
-      sx={{ backgroundColor: "#eeececff" }}
+      sx={{
+        backgroundColor: "white",
+        boxShadow: "5px 5px 15px rgba(219, 219, 219, 0.42)",
+        position: "relative",
+        zIndex: 1,
+      }}
     ></Box>
   );
 }
