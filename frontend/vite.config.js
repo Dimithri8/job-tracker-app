7e257c7a4d4e95c71d28293235e56f118dc341ca
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-13 13:07:30 +0530

    Fixed mui charts rendering issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "react-dom": path.resolve("./node_modules/react-dom"),
+      "@emotion/react": path.resolve("./node_modules/@emotion/react"),
+      "@emotion/styled": path.resolve("./node_modules/@emotion/styled"),
+    },
+  },
+});
