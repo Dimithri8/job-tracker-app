3ab19820518f98dd0f4f2f082479fd79dad871e9
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-14 17:28:58 +0530

    Created user schema with password hashing

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
