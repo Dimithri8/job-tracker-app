139edf0ffb00538dedb1bbba323e2cf39cbb9944
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-15 00:03:11 +0530

    Added protected post and get routes for jobs

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { createJob, getJobs } from "../controllers/jobController.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.post("/", protect, createJob);
+router.get("/", protect, getJobs);
 
 export default router;
