ef21561c4220356c8cc40afe01f9aba755f8859f
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-14 12:49:12 +0530

    Added nested routes to handle user auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Jobs from "./pages/Jobs/Jobs";
+import Interviews from "./pages/Interviews/Interviews";
+import Analytics from "./pages/Analytics/Analytics";
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="jobs" element={<Jobs />} />
+        <Route path="interviews" element={<Interviews />} />
+        <Route path="analytics" element={<Analytics />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,14 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
-import Dashboard from "../../pages/Dashboard/Dashboard";
-import Jobs from "../../pages/Jobs/Jobs";
-import Interviews from "../../pages/Interviews/Interviews";
-import Analytics from "../../pages/Analytics/Analytics";
 
 import { Box } from "@mui/material";
 import "./Layout.css";
+
 export default function Layout() {
   return (
     <Box className="layout-wrapper">
@@ -20,12 +17,7 @@ export default function Layout() {
           sx={{ backgroundColor: "#F8F8F8" }}
           className="content-wrapper"
         >
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/interviews" element={<Interviews />} />
-            <Route path="/analytics" element={<Analytics />} />
-          </Routes>
+          <Outlet />
         </Box>
       </Box>
     </Box>
