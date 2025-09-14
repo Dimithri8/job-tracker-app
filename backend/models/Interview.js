42c34ddbb9a145c8d16bad64a06b1ad6c81b6556
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-15 00:01:26 +0530

    Created interview schema

---
+++ backend/models/Interview.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const interviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    interviewDate: {
+      type: Date,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Interview = mongoose.model("Interview", interviewSchema);
+export default Interview;
