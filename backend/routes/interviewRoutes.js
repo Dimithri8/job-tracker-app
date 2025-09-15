475fc94f299909b38e65a7c89bb0cb8e153db6e9
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-15 16:58:49 +0530

    Fixed delete route typo

--- backend/routes/interviewRoutes.js
+++ backend/routes/interviewRoutes.js
@@ -4,12 +4,14 @@ import {
   updateInterview,
   deleteInterview,
   getInterviews,
-} from "../controllers/interviewController";
-import protect from "../middleware/authMiddleware";
+} from "../controllers/interviewController.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createInterview);
 router.put("/:id", protect, updateInterview);
 router.get("/", protect, getInterviews);
-router.delete(":id", protect, deleteInterview);
+router.delete("/:id", protect, deleteInterview);
+
+export default router;
