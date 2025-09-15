22c585cdf1e63308ba78df9127dfcc27aaa011b2
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-15 10:29:52 +0530

    Implemented update and delete job functionality

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -2,8 +2,7 @@ import Job from "../models/Job.js";
 
 export const createJob = async (req, res) => {
   try {
-    const { company, title, date, time, type, status } = req.body;
-    const interviewDate = new Date(`${date}T${time}`);
+    const { company, title, date, type, status, location, notes } = req.body;
 
     const existingJob = await Job.findOne({
       user: req.user.id,
@@ -17,9 +16,11 @@ export const createJob = async (req, res) => {
       user: req.user.id,
       title,
       company,
-      interviewDate,
+      appliedDate: date,
       type,
       status,
+      location,
+      notes,
     });
     res.status(201).json({
       message: "Job application created successfully",
@@ -40,6 +41,44 @@ export const getJobs = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Server error, couldn't get jobs",
+      error: error.message,
+    });
+  }
+};
+
+export const updateJob = async (req, res) => {
+  try {
+    const { company, title, date, type, status, location, notes } = req.body;
+
+    const updatedJob = await Job.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      { company, title, appliedDate: date, type, status, location, notes },
+      { new: true }
+    );
+    if (!updatedJob) return res.status(404).json({ message: "No job found" });
+    res
+      .status(200)
+      .json({ message: "Job application updated", job: updatedJob });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error, cannot update job application",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteJob = async (req, res) => {
+  try {
+    const deletedJob = await Job.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user.id,
+    });
+    if (!deletedJob)
+      return res.status(404).json({ message: "Job cannot be found" });
+    res.status(200).json({ message: "Job deleted", job: deletedJob });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error, cannot delete job",
       error: error.message,
     });
   }
