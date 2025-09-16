c9ba66680264dce58379cf4f96e5391b5a945c50
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-16 15:26:16 +0530

    Impletemented user password changing and profile image uploading functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,7 @@ dotenv.config();
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import { createToken } from "../utils/jwt.js";
+import cloudinary from "../config/cloudinary.js";
 
 export const registerUser = async (req, res) => {
   try {
@@ -19,7 +20,12 @@ export const registerUser = async (req, res) => {
     res.status(201).json({
       message: "Account created successfully",
       token,
-      user: { id: user._id, firstName, lastName, email },
+      user: {
+        id: user._id,
+        firstName,
+        lastName,
+        email,
+      },
     });
   } catch (error) {
     res.status(500).json({
@@ -47,11 +53,49 @@ export const loginUser = async (req, res) => {
         firstName: user.firstName,
         lastName: user.lastName,
         email,
+        profileImg: user.profileImg,
       },
     });
   } catch (error) {
     res.status(500).json({
       message: "Unable to process your request, please try again shortly",
+      error: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { firstName, lastName, email, password } = req.body;
+
+    const updatedFields = {};
+    if (firstName) updatedFields.firstName = firstName;
+    if (lastName) updatedFields.lastName = lastName;
+    if (email) updatedFields.email = email;
+    if (password && password.trim() !== "") {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      updatedFields.password = hashedPassword;
+    }
+    if (req.file) {
+      const uploadRes = await cloudinary.uploader.upload(req.file.path, {
+        folder: "job-tracker/users",
+      });
+      updatedFields.profileImg = uploadRes.secure_url;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: updatedFields },
+      { new: true }
+    ).select("-password");
+
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error, unable to update user",
       error: error.message,
     });
   }
